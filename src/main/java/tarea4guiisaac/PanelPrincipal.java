80aa0af93932b979d74d350b4834542c8fa3f053
Isaac <jimenezmerino.isaac9871@gmail.com>
2021-03-27 17:54:14 +0100

    introducir el contenido de 'initComponents'

--- src/main/java/tarea4guiisaac/PanelPrincipal.java
+++ src/main/java/tarea4guiisaac/PanelPrincipal.java
@@ -41,9 +41,32 @@ public class PanelPrincipal extends JPanel implements ActionListener {
         initComponents();
         tipoOperacion = -1; // No hay operaciones en la calculadora
     }
- // Se inicializan los componentes gráficos y se colocan en el panel
+    // Se inicializan los componentes gráficos y se colocan en el panel
+
     private void initComponents() {
 
+        // Creamos el panel de botones
+        botonera = new PanelBotones();
+        // Creamos las áreas de textos
+        //No tiene valores ni lo añadimos puesto que no es necesario que sea visible
+        areaTextoCalculos = new JTextArea();
+        areaTextoVisible = new JTextArea(10, 50);
+        //Para que no se pueda agrandar la ventana que se crea
+        areaTextoVisible.setEditable(false);
+        //Fondo de pantalla del area de texto
+        areaTextoVisible.setBackground(Color.white);
+        //Aumenta el tamaño de la letra del area de texto
+        areaTextoVisible.setFont(areaTextoVisible.getFont().deriveFont(20f));
+
+        //Establecemos layout del panel principal
+        this.setLayout(new BorderLayout());
+        // Colocamos la botonera y el área texto
+        this.add(areaTextoVisible, BorderLayout.NORTH);
+        this.add(botonera, BorderLayout.SOUTH);
+
+        for (JButton boton : this.botonera.getgrupoBotones()) {
+            boton.addActionListener((ActionListener) this);
+        }
     }
 
     @Override
